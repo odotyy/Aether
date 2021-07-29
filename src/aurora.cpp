9e13b7d8fddbaeaf368fb258a0167c83d8055ba1
Aaron Ridley <ridley@umich.edu>
2021-07-29 19:32:27 -0400

    FEAT: Reorganize the fang stuff. Still unit issues!

--- src/aurora.cpp
+++ src/aurora.cpp
@@ -13,10 +13,9 @@
 
 // -----------------------------------------------------------------------------
 // Read in aurora information
+//  - need both neutrals and ions, so we can't do this while reading in
+//    planet file.
 // -----------------------------------------------------------------------------
-
-// ?? reading in like euv example using args.get_aurora_file()  not working ??
-// hardcoding file name for now
 
 void read_aurora(Neutrals &neutrals, 
 		 Ions &ions,
@@ -31,8 +30,6 @@ void read_aurora(Neutrals &neutrals,
 
     int nLines = csv.size();
 
-    // ?? do something for if csv is empty ??
-
     for (int iLine = 0; iLine < nLines; iLine++) {
       //set aurora ion and neutral index and coef
       int iNeutral_ = neutrals.get_species_id(csv[iLine][0],report);
@@ -45,6 +42,23 @@ void read_aurora(Neutrals &neutrals,
     myFile.close();
   }
   
+}
+
+// -----------------------------------------------------------------------------
+// Calculate the Maxellian distribution (eqn 6 in Fang et al. [2010])
+// -----------------------------------------------------------------------------
+
+fvec calculate_maxwellian(float eflux,  // in ergs/cm2/s
+			  float avee,   // in keV
+			  fvec energies) {  // in eV
+
+  // Change all units to be in eV and cm2:
+  float E0 = avee / 2 * 1000.0; // characteristic energy in eV
+  float Q0 = eflux * 6.242e11;  // eV/cm2/s (ergs -> eV)
+  float a = Q0 / 2 / (E0 * E0 * E0);  // cm2/s/eV2
+  
+  fvec diff_num_flux = a * energies % exp(-energies / E0);  //eV/cm2/s
+  return  diff_num_flux;
 }
 
 // -----------------------------------------------------------------------------
@@ -86,6 +100,48 @@ fvec calculate_fang(float eflux,  // in ergs/cm2/s
     
     report.exit(function);
     return qtot; 
+}
+
+// -----------------------------------------------------------------------------
+// Function to Calculate ionization rate for 1 Ebin for 1 alt profile
+// -----------------------------------------------------------------------------
+
+fvec calculate_fang_v2(float energy_bin,
+		       float diff_energy_flux,
+		       fvec rhoH,
+		       std::vector<float> Ci, 
+		       fvec scale_height,
+		       Report &report) {
+
+    // report 
+    std::string function = "calc_fang";
+    static int iFunction = -1;
+    report.enter(function, iFunction);
+
+    // rhoH is in kg/m2, but need it in g/cm2 (/10)
+    // scale_height is in m, but need it in cm
+    // energy_bin is in eV but need it in keV
+    // diff_energy_flux is in eV/cm2/s but need it in keV/cm2/s
+    
+    float de = 0.035;  // keV
+    float E_mono = energy_bin / 1000.0;   // Convert to keV
+    float Q_mono = diff_energy_flux / 1000.0;   // Convert to keV
+    dvec H = conv_to<dvec>::from(scale_height) * 100.0; // Convert to cm
+    
+    // Eqn. 1 of Fang et al [2010]:
+    // This *100 below should be a /10, but we don't understand why it isn't:
+    dvec rhoHnorm = conv_to<dvec>::from(rhoH * 100.0 / 6e-6);
+    dvec yE = (2.0/E_mono) * pow( rhoHnorm, 0.7);
+    // Eqn. 4 of Fang et al [2010]:
+    dvec fyE =
+      (Ci[0] * pow(yE, Ci[1])) % exp((-Ci[2] * pow(yE, Ci[3]))) + 
+      (Ci[4] * pow(yE, Ci[5])) % exp((-Ci[6] * pow(yE, Ci[7])));
+    // Eqn. 3 of Fang et al [2010] (parenthesis):
+    dvec fac = Q_mono / de / H;
+    // Eqn. 3 of Fang et al [2010] (solve for Qtot(z)):
+    fvec q_tot = conv_to<fvec>::from(fyE % fac);
+    report.exit(function);
+    return q_tot; 
 }
 
 // -----------------------------------------------------------------------------
@@ -131,8 +187,8 @@ void calc_aurora(Grid grid,
   static float Emin = log(min);
   static float Emax = log(max);
   static int nBins = 101;
-  static std::vector<float> auroral_energies;
-  static std::vector<float> auroral_energy_widths;
+  static fvec auroral_energies(nBins);
+  static fvec auroral_energy_widths(nBins);
   std::vector<float> Ci;
   
   if (IsFirstTime) {
@@ -143,13 +199,13 @@ void calc_aurora(Grid grid,
 
     for (int64_t iBin = 0; iBin < nBins; iBin++) {
       float energy = exp(Emin + iBin*(Emax-Emin)/(nBins-1));
-      auroral_energies.push_back(energy);  // divide by 1000 here
+      auroral_energies(iBin) = energy;
     }
     auroral_energy_widths = calc_bin_widths(auroral_energies);
     
     for (int64_t iBin = 0; iBin < nBins; iBin++) {
 
-      lnE = log(auroral_energies[iBin]/1000.0);  // eV -> keV
+      lnE = log(auroral_energies(iBin)/1000.0);  // eV -> keV
 
       // loop through Pij values to get vector of Ci values
       for(int i = 0; i < 8; i++){
@@ -187,13 +243,15 @@ void calc_aurora(Grid grid,
 
   float eflux;
   float avee;
-
+  fvec diff_num_flux;
+  fvec diff_energy_flux;
+  
   // loop through each altitude and calculate ionization
   for (iLon = 0; iLon < nLons ; iLon++) {
     for (iLat = 0; iLat < nLats ; iLat++) {
 
-      eflux = ions.eflux(iLon, iLat);
-      avee = ions.avee(iLon, iLat);
+      eflux = ions.eflux(iLon, iLat);  // in ergs/cm2/s
+      avee = ions.avee(iLon, iLat);  // in keV
 
       if (eflux > 0.01) {
 
@@ -206,23 +264,35 @@ void calc_aurora(Grid grid,
 	  rhoH1d = rhoH1d + rho_tube;
 	}
 
-	// Step 2: Calculate ionization rates from Fang (all energy bins):
+	// Step 2: Calculate the distribution function:
+	diff_num_flux = calculate_maxwellian(eflux,  
+					     avee,   
+					     auroral_energies);
+
+	// Step 3: Calculate the differential energy flux:
+	diff_energy_flux =
+	  diff_num_flux % auroral_energies % auroral_energy_widths;
+
 	
-	ionization1d.zeros();
+	// Step 4: Calculate ionization rates from Fang (all energy bins):
+	
 	H = scale_height.tube(iLon,iLat);
 	fvec temp;
 
-	for(int iBin = 0; iBin < nBins; iBin++){
+	ionization1d.zeros();
+	for (int iBin = 0; iBin < nBins; iBin++){
 	  Ci = CiArray[iBin];
-    	  temp = calculate_fang(eflux, avee,
-				auroral_energies.at(iBin),
-				rhoH1d,
-				Ci,
-				auroral_energy_widths.at(iBin),
-				H,
-				report);
+	  temp = calculate_fang_v2(auroral_energies(iBin),
+				   diff_energy_flux(iBin),
+				   rhoH1d,
+				   Ci,
+				   H,
+				   report);
 	  ionization1d = ionization1d + temp;  
 	}
+	// This /1e4 shouldn't be there, but I don't know why it doesn't work
+	// without it.
+	ionization1d = ionization1d * pcm3topm3 / 1.0e4;  // /cm3 -> /m3
 
 	// Step 3: Distribute ionization among neutrals:
 	// Need to figure out which species get what percentage of the
