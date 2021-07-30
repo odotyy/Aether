492170f765eb4d62b11e0479c1bd94b7688449fd
Aaron Ridley <ridley@umich.edu>
2021-07-30 10:12:38 -0400

    FEAT: make a bulk (mass density weighted) full ion velocity vector

--- src/calc_ion_drift.cpp
+++ src/calc_ion_drift.cpp
@@ -93,22 +93,32 @@ void Ions::calc_ion_drift(Neutrals neutrals,
   int64_t iIon, iNeutral;
 
   std::vector<fcube> grad_Pi_plus_Pe;
-  fcube rho, rho_nuin, nuin_sum;
+  fcube rho, rho_nuin, nuin_sum, Nie, sum_rho;
 
   nuin_sum.set_size(nX, nY, nZ);
   nuin_sum.zeros();
+
+  sum_rho.set_size(nX, nY, nZ);
+  sum_rho.zeros();
   
   fill_electrons(report);
+
+  for (int64_t iComp = 0; iComp < 3; iComp++)
+    velocity_vcgc[iComp].zeros();
   
   for (iIon = 0; iIon < nIons; iIon++) {
 
-    // Only calculate the ion velocity for the species that are advected:
+    for (int64_t iComp = 0; iComp < 3; iComp++) {
+      species[iIon].perp_velocity_vcgc[iComp].zeros();
+      species[iIon].par_velocity_vcgc[iComp].zeros();
+    }
     
     if (species[iIon].DoAdvect) {
     
       // Need mass density for the current ion species:
       rho = species[iIon].mass * species[iIon].density_scgc;
-
+      Nie = cE * species[iIon].density_scgc;
+    
       // Get gradient in pressure:
       report.print(5, "going into pressure gradient");
       grad_Pi_plus_Pe = calc_ion_electron_pressure_gradient(iIon, grid, report);
@@ -136,42 +146,53 @@ void Ions::calc_ion_drift(Neutrals neutrals,
 	total_forcing[iComp] =
 	  - grad_Pi_plus_Pe[iComp]
 	  + gravity_vcgc[iComp]
-	  + wind_forcing[iComp];
+	  + wind_forcing[iComp]
+	  + Nie % efield_vcgc[iComp];
       }
 
       std::vector<fcube> a_par = make_cube_vector(nX, nY, nZ, 3);
+      std::vector<fcube> a_perp = make_cube_vector(nX, nY, nZ, 3);
+      std::vector<fcube> a_x_b;
       if (grid.get_HasBField()) {
+	// With a Planetary Magnetic field
 	fcube a_dot_b = dot_product(total_forcing, grid.bfield_unit_vcgc);
 	for (int64_t iComp = 0; iComp < 3; iComp++) {
 	  a_par[iComp] = a_dot_b % grid.bfield_unit_vcgc[iComp];
-	  // Steady state:
-	  species[iIon].par_velocity_vcgc[iComp] =
-	    a_par[iComp] / rho / nuin_sum;
-	  // implicit time-step
-	  //(species[iIon].par_velocity_vcgc[iComp] + a_par[iComp] * dt / rho) /
-	  //(1 + nuin_sum * dt);
+	  a_perp[iComp] = total_forcing[iComp] - a_par[iComp];
 	}
+	a_x_b = cross_product(a_perp, grid.bfield_vcgc);
+	for (int64_t iComp = 0; iComp < 3; iComp++)
+	  species[iIon].perp_velocity_vcgc[iComp] =
+	    (rho_nuin % a_perp[iComp] + Nie % a_x_b[iComp]) /
+	    (rho_nuin % rho_nuin +
+	     Nie % Nie % grid.bfield_mag_scgc % grid.bfield_mag_scgc);	
       } else {
+	// No Planetary Magnetic field
 	for (int64_t iComp = 0; iComp < 3; iComp++) {
 	  a_par[iComp] = total_forcing[iComp];
 	  // Steady state:
 	  species[iIon].par_velocity_vcgc[iComp] =
 	    a_par[iComp] / rho / nuin_sum;
-	  // implicit time-step
 	  //species[iIon].par_velocity_vcgc[iComp] = 
 	  //  (species[iIon].par_velocity_vcgc[iComp] + a_par[iComp] * dt / rho) /
 	  //  (1 + nuin_sum * dt);
 	}
-      }      
-      
-    } else {
-      for (int64_t iComp = 0; iComp < 3; iComp++) {
-	species[iIon].par_velocity_vcgc[iComp].zeros();
-	species[iIon].perp_velocity_vcgc[iComp].zeros();
       }
-    }
-  }
+      // Calculate the mass-weighted average total velocity
+      sum_rho = sum_rho + rho;
+      for (int64_t iComp = 0; iComp < 3; iComp++) {
+	velocity_vcgc[iComp] = velocity_vcgc[iComp] +
+	  rho % (species[iIon].perp_velocity_vcgc[iComp] +
+		 species[iIon].par_velocity_vcgc[iComp]);
+      }
 
+    }  // if DoAdvect
+
+  }  // for iIon
+
+  for (int64_t iComp = 0; iComp < 3; iComp++)
+    velocity_vcgc[iComp] = velocity_vcgc[iComp] / sum_rho;
+  
   report.exit(function);
   return;
 }
